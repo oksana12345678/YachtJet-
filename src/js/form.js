b548d5f6e69dc1b8c24e63f6ac94f29fc8e35f0b
oksana12345678 <oksana.tanashuk@gmail.com>
2023-12-23 22:16:50 +0100

    fix

--- src/js/form.js
+++ src/js/form.js
@@ -1,12 +1,6 @@
 const form = document.querySelector('#form');
 const launchBtn = document.querySelector('#launch-btn');
-const goToFormButton = document.querySelector('#go-to-form-btn');
 const userEmailField = document.querySelector('#user-email');
-
-goToFormButton.addEventListener('click', function (e) {
-  e.preventDefault();
-  form.scrollIntoView();
-});
 
 function clearFormFields() {
   const fieldName = form.querySelector('input[type="text"]');
@@ -15,9 +9,8 @@ function clearFormFields() {
 
   fieldName.value = '';
   fieldEmail.value = '';
+  fieldTel.value = '';
 }
-
-addGooseElement();
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -30,16 +23,5 @@ form.addEventListener('submit', e => {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => {
-      showGooseAnim();
-
-      launchBtn.removeAttribute('disabled');
-      clearFormFields();
-
-      setTimeout(() => {
-        launchBtn.style.opacity = '1';
-      }, 4000);
-    })
-    .catch(error => console.log('Sending form failed'));
+  });
 });
--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 import './js/isOpen.js';
+import './js/form.js';
